4319443758a9343130084db8453ebbb84548f58b
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-07-03 16:26:12 -0400

    palette style, allow html in lemmas

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const classes = ['lang', 'origin-lang', 'entry-name', 'source', 'gloss', 'word', 'origin', 'clade', 'notes', 'reflexes'];
-    const special_chars = ['ṭ', 'ḍ', 'ṣ', 'ṛ', 'r̩', 'ṁ', 'ʰ'];
+    const special_chars = ['ṭ', 'ḍ', 'ṣ', 'ṛ', 'r̩', 'r̆', 'ṁ', 'ʰ'];
     var pal = document.createElement("div");
     pal.classList.add("palette");
     pal.classList.add("hidden");
-    special_chars.forEach(char => {
+    special_chars.forEach((char, idx) => {
         var span = document.createElement("span");
         span.textContent = char;
         pal.appendChild(span);
